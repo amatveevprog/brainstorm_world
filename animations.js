03e1a0d6e294f405a55f77e1c56078f749ebbd09
Alex <avbondarik@gmail.com>
2015-11-12 20:06:00 +0300

    123456

--- animations.js
+++ animations.js
@@ -1,15 +1,17 @@
 // ИЗМЕНЕНИЕ КАТАЛОГА ОБЬЕКТОВ
 var Selected_Catalog_String = 'Чай / Улуны';
-var category_content_html;
+  var category_container = document.getElementById('startpagediv');
+  var category_container_short = document.getElementsByClassName("categoryshort")[0];
+  
 AddAnimationToCategory();
 
 function AddAnimationToCategory()
  {
    
-   var category_container = document.getElementsByClassName("category")[0];
-   category_content_html = category_container.innerHTML;
+  // var category_container = document.getElementsByClassName("category")[0];
+   //var short_category_container = document.getElementsByClassName("categoryshort")[0];
    
-   category_container.addEventListener("mouseenter",function(event)
+   category_container_short.addEventListener("mouseenter",function(event)
                   {
                        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
                        if (scrolled === 0)
@@ -17,23 +19,24 @@ function AddAnimationToCategory()
                          return; 
                         }
                       //console.log('enetr');
-                        var tween = TweenLite.to(category_container, 0.2,{height:'300px',color:'white'});
-                        category_container.innerHTML = category_content_html;
-                        console.log(category_container.innerHTML = category_content_html);
+                      category_container.style.display = 'block';
+                        //var tween = TweenLite.to(category_container, 0.2,{display:'block'});
+                       
                         
                         //category_container.innerHTML = '<hr/> OLOLOLOLPOJHJKHGHJYT';
                       
                   });
                   
-			category_container.addEventListener("mouseleave",function(event)
+			category_container_short.addEventListener("mouseleave",function(event)
                   {
                     var scrolled = window.pageYOffset || document.documentElement.scrollTop;
                     if (scrolled === 0)
                         {
                          return; 
                         }
-                    var tween = TweenLite.to(category_container, 0.2,{height:'40px',color:'white'});
-                    category_container.innerHTML = Selected_Catalog_String;
+                   // var tween = TweenLite.to(category_container, 0.2,{display:'none'});
+                    category_container.style.display = 'none';
+                   
                   });
  }
 
@@ -44,15 +47,15 @@ function AddAnimationToCategory()
 window.onscroll = function() 
  {
   var cart = document.getElementById('cartdiv');
-  //var category_container = document.getElementById('startpagediv');
-  var category_container = document.getElementsByClassName("category")[0];
+
   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
   console.log(scrolled + 'px');
   
   
   if(scrolled == 0)
    {
-    var tween = TweenLite.to(category_container, 0.5,{height:'300px',marginTop:'-30px',width:'100%',opacity:1,background:'white',color:'black',position:'relative',onStart:animcart});
+    var tween = TweenLite.to(category_container_short, 0.5,{height:'300px',marginTop:'-30px',width:'100%',opacity:1,background:'white',color:'black',position:'relative',onStart:animcart});
+    category_container.style.display = 'block';
     function animcart()
     {
      var tween = TweenLite.to(cart, 0.7,{opacity:0});
@@ -61,7 +64,8 @@ window.onscroll = function()
    }
    else
    {
-     var tween = TweenLite.to(category_container, 0.5,{height:'40px',marginTop:'0px',position:'fixed',top:'0px',width:'100%',opacity:0.7, background:'black',color:'white', onStart:hidecart});
+     var tween = TweenLite.to(category_container_short, 0.5,{height:'40px',marginTop:'0px',position:'fixed',top:'0px',width:'100%',opacity:0.7, background:'black',color:'white', onStart:hidecart});
+     category_container.style.display = 'none';    
      category_container.innerHTML = '<p style = "color:white; margin-left:20px">'+Selected_Catalog_String+'<p>';
       function hidecart() 
     {
