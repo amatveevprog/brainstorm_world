7baff2e8ca689ab5b36edd7eecf96090fd27ffcd
amatveevprog <alexey.matveev1@rambler.ru>
2015-11-15 22:09:52 +0300

    SPERMASPRULEN!!!

--- animations.js
+++ animations.js
@@ -3,15 +3,24 @@ var Selected_Catalog_String = 'Чай / Улуны';
   var category_container = document.getElementById('startpagediv');
   var category_container_short = document.getElementsByClassName("categoryshort")[0];
    var category = document.getElementsByClassName("category")[0];
+   var allProds = document.getElementById("allproducts");
+   var products = document.getElementsByClassName("content_container")[0];
+   
+   var specialDivForScrolling = document.createElement("div");
+   specialDivForScrolling.className = "categoryshort2";
+   specialDivForScrolling.innerText="1)Хлебные крошки 2)Хлебные крошки 3)Хлебные крошки";
+   category_container_short.appendChild(specialDivForScrolling);
+   specialDivForScrolling.style.display="none";
+   
 AddAnimationToCategory();
-
+var fl_appended=false;
 function AddAnimationToCategory()
  {
    
   // var category_container = document.getElementsByClassName("category")[0];
    //var short_category_container = document.getElementsByClassName("categoryshort")[0];
    
-   category_container_short.addEventListener("mouseenter",function(event)
+   /* category_container_short.addEventListener("mouseenter",function(event)
                   {
                        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
                        if (scrolled === 0)
@@ -36,11 +45,39 @@ function AddAnimationToCategory()
                         {
                          return; 
                         }
-                   // var tween = TweenLite.to(category_container, 0.2,{display:'none'});
+                   //var tween = TweenLite.to(category_container, 0.2,{display:'none'});
                     category.style.display = 'none';
                     category.style.position = 'relative';
                    
-                  });
+                  }); */
+	specialDivForScrolling.addEventListener("mouseenter",function(event)
+	{
+		//console.log("entered!!");
+		//show the derevo
+		displaychildren(specialDivForScrolling);
+	});
+	specialDivForScrolling.addEventListener("mouseleave",function(event)
+	{
+		//hide the tree
+		displayNONEchildren(specialDivForScrolling);
+		//console.log("leaved!!");
+	});
+	
+	
+	function displaychildren(ParentDiv)
+	{
+		for(var i=0;i<ParentDiv.children.length;i++)
+		{
+			ParentDiv.children[i].style.display="initial";
+		}
+	}
+	function displayNONEchildren(ParentDiv)
+	{
+		for(var i=0;i<ParentDiv.children.length;i++)
+		{
+			ParentDiv.children[i].style.display="none";
+		}
+	}
  }
 
 
@@ -64,16 +101,22 @@ window.onscroll = function()
      var tween = TweenLite.to(cart, 0.7,{opacity:0});
     }
      //cart.style.marginTop = '250px';
+	 allProds.insertBefore(category,products);
    }
    else
    {
      var tween = TweenLite.to(category_container_short, 0.5,{height:'40px',marginTop:'0px',position:'fixed',top:'0px',width:'100%',opacity:0.7, background:'black',color:'white', onStart:hidecart});
-     category.style.display = 'none';    
-     category_container.innerHTML = '<p style = "color:white; margin-left:20px">'+Selected_Catalog_String+'<p>';
+     category.style.display = 'block';    
       function hidecart() 
     {
      var tween = TweenLite.to(cart, 0.7,{opacity:1});
     }
      //cart.style.marginTop = '0px';
+	
+	specialDivForScrolling.style.display="block";
+	specialDivForScrolling.style.height = "40px";
+	specialDivForScrolling.style.width = category_container_short.style.width;
+	category.style.display="none";
+	specialDivForScrolling.appendChild(category); 
    }
  }
